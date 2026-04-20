9779df6c75ec88ada19ac7a565ab9422b30fc264
Anjali Kumari <anjalikeshav130@Gmail.com>
2026-04-20 22:34:41 +0530

    Create inline.cpp

---
+++ C++/Basics/Functions/inline.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+// Inline function
+inline int add(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int x = 5, y = 10;
+
+    cout << "Sum = " << add(x, y) << endl;
+
+    return 0;
+}
