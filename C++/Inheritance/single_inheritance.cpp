7e0111e6933803552c7f05eed6826cffd1c2fc57
Anjali Kumari <anjalikeshav130@Gmail.com>
2026-04-20 22:45:02 +0530

    Create single_inheritance.cpp

---
+++ C++/Inheritance/single_inheritance.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class Parent {
+public:
+    void show() {
+        cout << "This is Parent class" << endl;
+    }
+};
+
+class Child : public Parent {
+public:
+    void display() {
+        cout << "This is Child class" << endl;
+    }
+};
+
+int main() {
+    Child c;
+    c.show();
+    c.display();
+    return 0;
+}
